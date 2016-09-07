2220d6731ba1be768ae10ba6d9b56b191c31385c
zhangqiang <1069507276@qq.com>
2016-09-07 16:50:10 +0800

    测试

---
+++ Tst2/Tst2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tst2
+//
+//  Created by zhangqiang on 16/9/7.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tst2/Tst2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tst2
+//
+//  Created by zhangqiang on 16/9/7.
+//  Copyright © 2016年 jyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
